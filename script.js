e3ebc541f64cfe4d47eae22794f4892bce2a2b75
Dev <devpancholigt2004@gmail.com>
2025-06-19 01:06:36 +0530

    updated the css for the about section and js also

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ const themeToggle = document.getElementById('themeToggle');
 const themeIcon = document.getElementById('themeIcon');
 const html = document.documentElement;
 
-// Set theme based on saved preference
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme === 'dark') {
+// Set initial theme
+if (localStorage.getItem('theme') === 'dark') {
     html.setAttribute('data-bs-theme', 'dark');
     themeIcon.classList.replace('fa-moon', 'fa-sun');
-    themeIcon.classList.replace('text-dark', 'text-light');
 }
 
 themeToggle.addEventListener('click', () => {
@@ -19,11 +17,9 @@ themeToggle.addEventListener('click', () => {
         html.setAttribute('data-bs-theme', 'dark');
         localStorage.setItem('theme', 'dark');
         themeIcon.classList.replace('fa-moon', 'fa-sun');
-        themeIcon.classList.replace('text-dark', 'text-light');
     } else {
         html.setAttribute('data-bs-theme', 'light');
         localStorage.setItem('theme', 'light');
         themeIcon.classList.replace('fa-sun', 'fa-moon');
-        themeIcon.classList.replace('text-light', 'text-dark');
     }
 });
